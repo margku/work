7c517e7076902450e8bc989a8b5627e0b27d2545
margku <64085314+margku@users.noreply.github.com>
2020-08-02 22:55:54 +0300

    Add files via upload

---
+++ alphaZero/run/config.py
@@ -0,0 +1,30 @@
+#### SELF PLAY
+EPISODES = 15
+MCTS_SIMS = 10
+MEMORY_SIZE = 1000
+TURNS_UNTIL_TAU0 = 10 
+CPUCT = 1
+EPSILON = 0.2
+ALPHA = 0.8
+
+
+#### RETRAINING
+BATCH_SIZE = 128
+EPOCHS = 1
+REG_CONST = 0.0001
+LEARNING_RATE = 0.1
+MOMENTUM = 0.9
+TRAINING_LOOPS = 10
+
+HIDDEN_CNN_LAYERS = [
+	{'filters':75, 'kernel_size': (4,4)}
+	 , {'filters':75, 'kernel_size': (4,4)}
+	 , {'filters':75, 'kernel_size': (4,4)}
+	 , {'filters':75, 'kernel_size': (4,4)}
+	 , {'filters':75, 'kernel_size': (4,4)}
+	 , {'filters':75, 'kernel_size': (4,4)}
+	]
+
+#### EVALUATION
+EVAL_EPISODES = 10
+SCORING_THRESHOLD = 1.3
